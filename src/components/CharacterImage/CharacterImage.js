d93fe48e3b682b78a554101cb668c4901fcfa46f
Enigma <jonpking@gmail.com>
2019-09-23 21:26:25 -0700

    added components for title, wrapper, characterimage

---
+++ src/components/CharacterImage/CharacterImage.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+class CharacterImage extends React.Component {
+    state = {
+        count: 0
+    };
+
+    render() {
+        return (
+            <div className="">
+
+            </div>
+        );
+    }
+}
+
+export default CharacterImage;
