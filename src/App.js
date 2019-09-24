c6305f438ab536584fafa771ed21296917d370b7
Enigma <jonpking@gmail.com>
2019-09-23 21:45:53 -0700

    updated CharacterImage/CharacterImage.js to CharacterImage/index.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 import Wrapper from "./components/Wrapper";
 import Title from "./components/Title";
-import CharacterImage from "./components/CharacterImage/CharacterImage.js";
+import CharacterImage from "./components/CharacterImage";
 import character from "./characters.json";
 
 function App() {
